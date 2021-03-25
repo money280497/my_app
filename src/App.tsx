7858f2b30f58e2d183a0c40d55ce6460e6d30487
Mohit Mehta <Mohit.Mehta@Aurigo.com>
2021-03-26 00:39:15 +0530

    style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
-import Comp from './api/Data';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import Comp from "./api/Data";
+import "./styles/main.css";
 
 interface IPost {
-  id: number;
+  id: [];
+}
+interface ITitle {
+  title: [];
 }
 interface IPostItems {
-    by: string,
-    descendants: number,
-    id: number,
-    kids: [],
-    score: number,
-    time: number,
-    title: string,
-    type: string,
-    url: string
+  by: string;
+  descendants: number;
+  id: number;
+  kids: [];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
 }
-const PostItems : IPostItems ={
+const PostItems: IPostItems = {
   by: "",
   descendants: 0,
   id: 0,
@@ -26,51 +30,87 @@ const PostItems : IPostItems ={
   time: 0,
   title: "",
   type: "",
-  url: ""
-}
+  url: "",
+};
 
 const defaultPosts: IPost[] = [];
-const App: React.FC= () => {
+const defaultTitle: ITitle[] = [];
+const App: React.FC = () => {
   const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(
     defaultPosts
   );
+  const [title, setTitle]: [
+    ITitle[],
+    (posts: ITitle[]) => void
+  ] = React.useState(defaultTitle);
   const [flag, setFlag] = React.useState<boolean>(false);
-  const [data, setData]= React.useState<IPostItems>(PostItems
-  );
-  React.useEffect(()=>{
-    const url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
-    axios.get<IPost[]>(url)
-    .then(response => {
-        //console.log(response.data);
-        setPosts( response.data );
+  const [data, setData] = React.useState<IPostItems>(PostItems);
+  React.useEffect(() => {
+    const url =
+      "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+    axios.get<IPost[]>(url).then((response) => {
+      //console.log(response.data);
+      setPosts(response.data.slice(0, 50));
+      let promises = response.data.slice(0, 50).map((item) => {
+        return axios.get(
+          `https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`
+        );
+      });
+      Promise.all(promises).then((res) =>
+        setTitle(
+          res.map((item) => {
+            console.log(item.data.title);
+            return item.data.title;
+          })
+        )
+      );
     });
-  },[])
-  const handleClick = (item: object) =>{
+  }, []);
+
+  const handleClick = (item: object) => {
     const current = item;
-      const url = `https://hacker-news.firebaseio.com/v0/item/${current}.json?print=pretty`;
-      axios.get<IPostItems>(url)
-      .then(response => {
-          setFlag(true);
-          setData(response.data);
-      }); 
-  }
+    const url = `https://hacker-news.firebaseio.com/v0/item/${current}.json?print=pretty`;
+    axios.get<IPostItems>(url).then((response) => {
+      setFlag(true);
+      setData(response.data);
+    });
+  };
   //console.log("mohit",data);
 
   return (
-    <div className="App">
-      {!!flag ?
-      <Comp {...data} />
-      :(
-      posts.map((item)=>{
-     return <React.Fragment>
-     <h5>{item}</h5>
-     <button onClick={(event: React.MouseEvent<HTMLElement>) => {
-   handleClick(item)
-  }}>{item}</button> 
-     </React.Fragment>
-    }
-    ))
-    }
+    <div className="bg-coolGray">
+      {!!flag ? (
+        <>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 m-auto item-center text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={(event: React.MouseEvent<HTMLElement>) => {
+              setFlag(false);
+            }}
+          >
+            Back
+          </button>
+          <Comp {...data} />
+        </>
+      ) : (
+        <div className="grid gap-3 grid-cols-8 grid-flow-row auto-row-max">
+          {title.map((item, index) => {
+            return (
+              <div>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                  onClick={(event: React.MouseEvent<HTMLElement>) => {
+                    handleClick(posts[index]);
+                  }}
+                >
+                  {item}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/api/Data.tsx
+++ src/api/Data.tsx
@@ -1,30 +1,79 @@
-import React from 'react';
-import  App from '../App';
+import React from "react";
+import App from "../App";
 interface Props {
-        by: string,
-        descendants: number,
-        id: number,
-        kids: [],
-        score: number,
-        time: number,
-        title: string,
-        type: string,
-        url: string
+  by: string;
+  descendants: number;
+  id: number;
+  kids: [];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
 }
-  
-const Comp:React.FC<Props> = (props): JSX.Element => {
-    return (
-    <div className="App">    
-     <h1>{props.by}</h1>
-     <h1>{props.descendants}</h1>
-     <h1>{props.id}</h1>
-     <h1>{props.kids}</h1>
-     <h1>{props.score}</h1>
-     <h1>{props.time}</h1>
-     <h1>{props.title}</h1>
-     <h1>{props.type}</h1>
-     <h1>{props.url}</h1>  
-   </div>);
-  };
+
+const Comp: React.FC<Props> = (props): JSX.Element => {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="max-w-md mx-auto">
+            <div>
+              <b>
+                <i>
+                  <p className="h-7 sm:h-8">{props.type.toLocaleUpperCase()}</p>
+                </i>
+              </b>
+            </div>
+            <div className="divide-y divide-gray-200">
+              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <p>{props.title}</p>
+                <ul className="list-disc space-y-2">
+                  {props.kids.length &&
+                    props.kids.map((item) => (
+                      <li className="flex items-start">
+                        <span className="h-6 flex items-center sm:h-7">
+                          <svg
+                            className="flex-shrink-0 h-5 w-5 text-cyan-500"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                        <p className="ml-2">{item}</p>
+                      </li>
+                    ))}
+                </ul>
+                <p>{}</p>
+                <b>
+                  <i>
+                    <p>{new Date(props.time * 1000).toDateString()}</p>
+                  </i>
+                </b>
+              </div>
+              <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+                <p>BY : {props.by}</p>
+                <p>
+                  <a
+                    href={props.url}
+                    className="text-cyan-600 hover:text-cyan-700"
+                  >
+                    Know More &rarr;
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Comp;
